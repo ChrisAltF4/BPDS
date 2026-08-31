3db38c3332756fd86c5e2524a3480db26add2adb
ChrisAltF4 <christianmarquezsandoval2008@gmail.com>
2026-08-30 21:35:52 -0500

    CRUD complete Create, Read, Update, Deleate and Edit

---
+++ Components/Filters/Todofilters.components.tsx
@@ -0,0 +1,45 @@
+const FiltersContainer= ({ children }: { children?: React.ReactNode }) => {
+  return (
+  <div className="flex item-center justify-between p-4 bg-gray-700 border-b border-solid-600">
+    {children}
+    </div>
+  )
+}
+
+const ItemsLeft = ({ total = 0 }) => (
+    <p className="text-gray-400 text-sm">
+        {total} item left
+    </p>
+)
+
+const FilterButtonContainer = ({ children }: { children?: React.ReactNode }) => {
+    return (
+    <div className="flex items-center space-x-2">
+        {children}
+    </div>
+    )
+}
+
+const FilterButton = ({
+  action,
+  active,
+  filter,
+}: {
+  action: () => void;
+  active: string;
+  filter: string;
+}) => {
+  return (
+    <button
+      onClick={action}
+      className={
+        "hover:text-white cursor-pointer transition-all duration-300 ease-in-out" +
+        (active.toLowerCase().includes(filter.toLowerCase()) ? " text-blue-400" : " text-gray-400")
+      }
+    >
+      {filter}
+    </button>
+  );
+};
+
+export { FiltersContainer, ItemsLeft, FilterButtonContainer, FilterButton }
--- app/page.tsx
+++ app/page.tsx
@@ -1,69 +1,124 @@
-import Image from "next/image";
+"use client"
+
+import { Title } from "@/Components/Title";
+import { Todo } from "@/Components/Todo";
+import { TodoInput } from "@/Components/Todoinput";
+import { Todolist } from "@/Components/Todolist";
+import { todo } from "node:test";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [todos, setTodo] = useState([
+    {
+      id: 1,
+      title: "Verme toda la serie del mentalista",
+      completed: false,
+    },
+    {
+      id: 2,
+      title: "Entregar la todo list",
+      completed: false,
+    },
+    {
+      id: 3,
+      title: "Estudiar Calculo diferencial",
+      completed: false,
+    },
+    {
+      id: 4,
+      title: "Saludar a la profe en clase",
+      completed: false,
+    },
+  ]);
+
+  const [activeFilter, setActiveFilter] =useState('all')
+  const [filteredTodos, setFilteredTodos] =useState (todos)
+
+  const addTodo = (title: string) => {
+    const lastId = todos.length > 0 ? todos [todos.length- 1].id : 1;
+
+    const newTodo = {
+      id: lastId + 1,
+      title,
+      completed: false
+    } 
+
+    const todoList =[...todos]
+    todoList.push(newTodo);
+    setTodo(todoList);
+  }
+
+  const handleSetCompleted = (id: number) => {
+    const updateList = todos.map (todo => {
+      if (todo.id === id) {
+        return {...todo, completed: !todo.completed}
+      }
+      return todo;
+    })
+    setTodo(updateList);
+  }
+  const handleDelete = (id: number) => {
+    const updateList = todos.filter(todo => todo.id !== id)
+    setTodo(updateList)
+  }
+  
+  const handleEditTodo = (id: number, newTitle: string) => {
+  const updateList = todos.map((todo) => {
+    if (todo.id === id) {
+      return { ...todo, title: newTitle };
+    }
+    return todo;
+  });
+  setTodo(updateList);
+};
+
+  const handleClearComplete = () => {
+  const updateList = todos.filter(todo => !todo.completed);
+  setTodo(updateList);
+  }
+
+  const showAllTodos = () => {
+    setActiveFilter('all')
+  }
+
+  const showActiveTodos = () => {
+    setActiveFilter('active')
+  }
+
+  const showCompletedTodos = () => {
+    setActiveFilter('completed')
+  }
+
+  useEffect(() =>{
+    if(activeFilter === 'all') {
+      setFilteredTodos(todos)
+    } else if ( activeFilter === 'active') {
+      const activeTodos = todos.filter(todo => todo.completed === false)
+      setFilteredTodos(activeTodos)
+    } else if (activeFilter === 'completed') {
+      const completedTodos = todos.filter(todo => todo.completed === true)
+      setFilteredTodos(completedTodos)
+    }
+  }, [activeFilter, todos]
+)
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert h-5 w-[100px]"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the{" "}
-            <code className="rounded bg-black/[.06] px-1.5 py-0.5 font-mono text-[0.9em] dark:bg-white/[.08]">
-              page.tsx
-            </code>{" "}
-            file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert h-[14px] w-4"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={14}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div className="bg-gray-900 min-h-screen h-full text-amber-300 flex items-center justify-center py-20 px-5">
+      <div className="container flex flex-col max-w-xl">
+        <Title />
+        <TodoInput addTodo={addTodo} />
+        <Todolist
+        todos={filteredTodos}
+        activeFilter={activeFilter}
+        handleSetCompleted={handleSetCompleted}
+        handleDelete={handleDelete}
+        handleEditTodo={handleEditTodo}
+        showAllTodos={showAllTodos}
+        showActiveTodos={showActiveTodos}
+        showCompletedTodos={showCompletedTodos}
+        handleClearComplete={handleClearComplete}
+/>
+      </div>
     </div>
   );
 }
