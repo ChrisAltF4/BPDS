116794d166337c359496345dcc8d9492a35f6cef
Fiorella Aragón <aragon.r.fiorella@gmail.com>
2026-09-07 20:40:11 -0500

    feat: add filter button component

--- components/Filters/Todofilters.components.tsx
+++ components/Filters/Todofilters.components.tsx
@@ -23,3 +23,25 @@ const FilterButtonContainer = ({ children }: { children?: React.ReactNode }) => 
         </div>
     );
 }
+
+const FilterButton = ({
+    action,
+    active,
+    filter,
+}: {
+    action: () => void;
+    active: string;
+    filter: string;
+}) => {
+    return (
+        <button
+            onClick={action}
+            className={
+                "hover:text-white cursor-pointer transition-all duration-300 ease-in-out" +
+                (active.toLowerCase().includes(filter.toLowerCase()) ? " text-blue-400" : " text-gray-400")
+            }
+        >
+            {filter}
+        </button>
+    );
+};
